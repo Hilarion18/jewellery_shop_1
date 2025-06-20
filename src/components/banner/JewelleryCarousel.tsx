ce99ee788f041bb91a4f79b188cb564cfc1e7819
hilarion <hilarion_cross@hotmail.com>
2025-06-20 10:41:11 +0800

    added banner

--- src/components/banner/JewelleryCarousel.tsx
+++ src/components/banner/JewelleryCarousel.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
-import Diamond3D from "./Diamond3d";
+import Diamond3D from "../../widget/Diamond3d.jsx";
 
 const banners = [
   {
